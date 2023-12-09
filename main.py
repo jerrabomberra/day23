9b6ea0f3529cc5a6b9f45281d7b9c8957bfbb44a
Doug Gordon <gordononline@bigpond.com>
2023-12-10 00:13:14 +1100

    Move turtle working

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from player import Player
 # from car_manager import CarManager
 # from scoreboard import Scoreboard
 
+player = Player()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title("Road crossing")
 
-player = Player()
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    screen.onkeypress(player.move(), "Up")
 
 
 screen.exitonclick()
